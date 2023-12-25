dca5aff2025623c7e6eac815cd9fcec3d8bdf8f9
Anne <anne.klapwijk@gmail.com>
2023-12-25 23:07:22 +0100

    Fix solution to basic recursion

--- functional-javascript/7_basic_recursion.js
+++ functional-javascript/7_basic_recursion.js
@@ -5,9 +5,10 @@
  * @param initial
  */
 function reduce(arr, fn, initial) {
-  if (!arr.length) return initial
-  const head = arr.shift()
-  return reduce(arr, fn, fn(initial, head))
+  return (function reduceOne(index, value) {
+    if (index > arr.length - 1) return value // end condition
+    return reduceOne(index + 1, fn(value, arr[index], index, arr)) // calculate & pass values to next step
+  })(0, initial) // IIFE. kick off recursion with initial values
 }
 
 module.exports = reduce
